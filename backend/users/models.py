b89e19d74306366725c894d2e55c8c30504df595
suroh <roh7891backup@gmail.com>
2026-05-12 19:45:08 +0100

    made temporary rule for user creation

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,6 +5,10 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)  # 
-
-    REQUIRED_FIELDS = ["email"]
+    # Django email model
+    # Email is optional at the model/database level for now.
+    # Later, normal user signup logic should require email.
+    email = models.EmailField(unique=True, blank=True, null=True)
+    # unique=True -> users cannot have the same email
+    # blank=True -> Django forms/admin are allowed to leave this field empty
+    # null=True -> To allow the database to add null value as well
