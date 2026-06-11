d6e9c2fe8e43cf8dd764e13c35c8ec8c383cbc3e
IDsuroh <roh7891backup@gmail.com>
2026-06-11 23:17:20 +0900

    Added notes for presentation

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+# Creates the actual Django WSGI application object. WSGI (Web Server Gateway Interface) standard connection format that lets Gunicorn call a Django application.
+# Its job is to expose the Django application to Gunicorn.
